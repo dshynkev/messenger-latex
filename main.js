2ed1377d6b99c9150a89e28c7f9b6960ec21766b
Dmytro Shynkevych <dm.shynk@gmail.com>
2017-06-21 01:28:28 -0400

    MathJax added

---
+++ main.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/CommonHTML"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    }
+}
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "//cdn.mathjax.org/mathjax/latest/MathJax.js";
+script.src = chrome.extension.getURL("MathJax/MathJax.js");
+document.getElementsByTagName("head")[0].appendChild(script);
+console.log("LaTeX loaded");
