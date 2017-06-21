653fef0a88f52958cf9bef3b1383c60624920bc6
Dmytro Shynkevych <dm.shynk@gmail.com>
2017-06-21 01:43:12 -0400

    Refactored setup code

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-window.MathJax = {
-    extensions: ["tex2jax.js"],
-    jax: ["input/TeX", "output/CommonHTML"],
-    tex2jax: {
-      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-      processEscapes: true
-    }
-}
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src = chrome.extension.getURL("MathJax/MathJax.js");
-document.getElementsByTagName("head")[0].appendChild(script);
-console.log("LaTeX loaded");
+(function() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = chrome.extension.getURL("MathJax/MathJax.js");
+
+    var config = "MathJax.Hub.Config({" +
+        "extensions: ['tex2jax.js']," +
+        "jax: ['input/TeX', 'output/CommonHTML']," +
+        "tex2jax: {" +
+            "inlineMath: [ ['$','$'], ['\\(','\\)'] ]," +
+            "displayMath: [ ['$$','$$'], ['\\[','\\]'] ]," +
+            "processEscapes: true" +
+        "}" +
+    "});" +
+    "MathJax.Hub.Startup.onload();";
+
+    script.text = config;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    console.log("LaTeX loaded");
+})();
