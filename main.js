03750f6985b6f14c5852b98ceb1623a3467d216a
Dmytro Shynkevych <dm.shynk@gmail.com>
2017-06-22 00:43:12 -0400

    Auto-refresh every 1000ms added

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
     script.src = chrome.extension.getURL("MathJax/MathJax.js");
     script.src += "?config=TeX-AMS_HTML"
 
+    // FIXME: update only when new a message is received instead
+    script.text = "setInterval(function() { MathJax.Hub.Queue(['Typeset',MathJax.Hub]); }, 1000);";
+
     document.getElementsByTagName("head")[0].appendChild(script);
     console.log("LaTeX loaded");
 })();
