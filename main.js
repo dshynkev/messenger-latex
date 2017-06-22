bb665bdce30e6e34ba70404a83b39ebd8a5a5a18
Dmytro Shynkevych <dm.shynk@gmail.com>
2017-06-21 23:33:06 -0400

    Switched from CommonHTML to HTML-CSS

--- main.js
+++ main.js
@@ -2,19 +2,8 @@
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.src = chrome.extension.getURL("MathJax/MathJax.js");
+    script.src += "?config=TeX-AMS_HTML"
 
-    var config = "MathJax.Hub.Config({" +
-        "extensions: ['tex2jax.js']," +
-        "jax: ['input/TeX', 'output/CommonHTML']," +
-        "tex2jax: {" +
-            "inlineMath: [ ['$','$'], ['\\(','\\)'] ]," +
-            "displayMath: [ ['$$','$$'], ['\\[','\\]'] ]," +
-            "processEscapes: true" +
-        "}" +
-    "});" +
-    "MathJax.Hub.Startup.onload();";
-
-    script.text = config;
     document.getElementsByTagName("head")[0].appendChild(script);
     console.log("LaTeX loaded");
 })();
