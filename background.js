e15139edd2c065c88b5a926a90e0c601e6e1cbb8
bretsky <bretskyselby99@gmail.com>
2017-06-23 14:07:25 -0700

    Change webRequest listener to onCompleted

--- background.js
+++ background.js
@@ -1,12 +1,11 @@
 // Request a refresh when the page changes
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
+chrome.webRequest.onCompleted.addListener(function(details) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   		chrome.tabs.sendMessage(tabs[0].id, "refresh");
 	});
 }, {urls: [
     "https://www.facebook.com/messaging/send/?dpr=1",
     "https://www.facebook.com/ajax/mercury/mark_seen.php?dpr=1",
-    "https://www.facebook.com/ajax/messaging/typ.php?dpr=1",
     "https://www.facebook.com/ajax/bz",
     "https://www.facebook.com/api/graphqlbatch/",
     "https://www.messenger.com/messaging/send/?dpr=1",
