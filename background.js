8bec0da3266031aa643d096f33085fb0ff9be3e0
bretsky <bretskyselby99@gmail.com>
2017-06-22 22:04:18 -0700

    Add listening for messenger.com

--- background.js
+++ background.js
@@ -4,4 +4,4 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     		console.log(response);
   		});
 	});
-}, {urls:["https://www.facebook.com/messaging/send/?dpr=1", "http://www.facebook.com/messaging/send/?dpr=1", "https://www.facebook.com/ajax/mercury/mark_seen.php?dpr=1", "http://www.facebook.com/ajax/mercury/mark_seen.php?dpr=1"]})
+}, {urls:["https://www.facebook.com/messaging/send/?dpr=1", "https://www.facebook.com/ajax/mercury/mark_seen.php?dpr=1", "https://www.facebook.com/ajax/messaging/typ.php?dpr=1", "https://www.messenger.com/messaging/send/?dpr=1", "https://www.messenger.com/ajax/mercury/delivery_receipts.php?dpr=1"]})
