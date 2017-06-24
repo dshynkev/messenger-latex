77cfd81d9ea8c58109820a5accc02974ef8b6209
Mathieu <matgodin@videotron.ca>
2017-06-24 01:03:00 -0400

    commit

--- mytest.js
+++ mytest.js
@@ -9,6 +9,37 @@ window.MathJax = {
   },
   TeX: {
     extensions: ["AMSmath.js", "AMSsymbols.js", "autoload-all.js"]
+  },
+  AuthorInit: function() {
+    MathJax.Hub.Register.StartupHook("End", function() {
+      var mj2img = function(texstring, callback) {
+        var input = texstring;
+        var wrapper = document.createElement("div");
+        wrapper.innerHTML = input;
+        var output = { svg: "", img: ""};
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, wrapper]);
+        MathJax.Hub.Queue(function() {
+          var mjOut = wrapper.getElementsByTagName("svg")[0];
+          mjOut.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+          // thanks, https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
+          output.svg = mjOut.outerHTML;
+          var image = new Image();
+          image.src = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(output.svg)));
+          image.onload = function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            var context = canvas.getContext('2d');
+            context.drawImage(image, 0, 0);
+            output.img = canvas.toDataURL('image/png');
+            callback(output);
+          };
+        });
+      }
+      mj2img("\\[f: X \\to Y\\]", function(output){
+        document.getElementById("target").innerText = output.img + '\n' + output.svg;
+      });
+    });
   }
 };
 
