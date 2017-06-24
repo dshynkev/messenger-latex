6488425c7540866ec5319c89edd27597e129dba1
bretsky <bretskyselby99@gmail.com>
2017-06-24 01:29:55 -0700

    Get SVG to PNG working for equations

--- png2.js
+++ png2.js
@@ -1,39 +1,42 @@
-var svg = document.querySelector('svg');
-var canvas = document.querySelector('canvas');
+setTimeout(function() {
+  var svg = document.getElementById('MathJax-Element-1-Frame').childNodes[0];
+  var canvas = document.querySelector('canvas');
+  console.log(svg)
 
-function triggerDownload (imgURI) {
-  var evt = new MouseEvent('click', {
-    view: window,
-    bubbles: false,
-    cancelable: true
-  });
+  function triggerDownload (imgURI) {
+    var evt = new MouseEvent('click', {
+      view: window,
+      bubbles: false,
+      cancelable: true
+    });
 
-  var a = document.createElement('a');
-  a.setAttribute('download', 'MY_COOL_IMAGE.png');
-  a.setAttribute('href', imgURI);
-  a.setAttribute('target', '_blank');
+    var a = document.createElement('a');
+    a.setAttribute('download', 'MY_COOL_IMAGE.png');
+    a.setAttribute('href', imgURI);
+    a.setAttribute('target', '_blank');
 
-  a.dispatchEvent(evt);
-}
+    a.dispatchEvent(evt);
+  }
 
-var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var data = (new XMLSerializer()).serializeToString(svg);
-  var DOMURL = window.URL || window.webkitURL || window;
+  var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var data = (new XMLSerializer()).serializeToString(svg);
+    var DOMURL = window.URL || window.webkitURL || window;
 
-  var img = new Image();
-  var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
-  var url = DOMURL.createObjectURL(svgBlob);
+    var img = new Image();
+    var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+    var url = DOMURL.createObjectURL(svgBlob);
 
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0);
-    DOMURL.revokeObjectURL(url);
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+      DOMURL.revokeObjectURL(url);
 
-    var imgURI = canvas
-        .toDataURL('image/png')
-        .replace('image/png', 'image/octet-stream');
+      var imgURI = canvas
+          .toDataURL('image/png')
+          .replace('image/png', 'image/octet-stream');
 
-    triggerDownload(imgURI);
-  };
+      triggerDownload(imgURI);
+    };
 
-  img.src = url;
+    img.src = url;
+}, 1000)
