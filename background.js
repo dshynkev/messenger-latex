1cb42996f601ee36fb69cf95286abd65a0e28ac3
bretsky <bretskyselby99@gmail.com>
2017-06-25 12:46:28 -0700

    Fix URL matching

--- background.js
+++ background.js
@@ -1,17 +1,39 @@
-var FSEND = "https://www.facebook.com/messaging/send/?dpr=1.5"
-var FRECEIVE = "https://www.facebook.com/ajax/mercury/mark_seen.php?dpr=1.5"
+var FSEND = "https://www.facebook.com/messaging/send/*"
+var FRECEIVE = "https://www.facebook.com/ajax/mercury/mark_seen.php*"
 var FSWITCH = "https://www.facebook.com/ajax/bz"
 var FSCROLL = "https://www.facebook.com/api/graphqlbatch/"
-var MSEND = "https://www.messenger.com/messaging/send/?dpr=1"
-var MRECEIVE = "https://www.messenger.com/ajax/mercury/delivery_receipts.php?dpr=1.5"
+var MSEND = "https://www.messenger.com/messaging/send/*"
+var MRECEIVE = "https://www.messenger.com/ajax/mercury/delivery_receipts.php*"
 var MSWITCH = "https://www.messenger.com/ajax/bz"
 var MSCROLL = "https://www.messenger.com/api/graphqlbatch/"
-var FTAB = "https://www.facebook.com/ajax/mercury/tabs_presence.php?dpr=1.5"
+var FTAB = "https://www.facebook.com/ajax/mercury/tabs_presence.php*"
+
+URLS = [
+    FSEND,
+    FRECEIVE,
+    FSWITCH,
+    FSCROLL,
+    MSEND,
+    MRECEIVE,
+    MSWITCH,
+    MSCROLL,
+    FTAB,
+];
+
+function checkURL(url) {
+    for (var i = 0; i < URLS.length; i++) {
+        if (url.includes(URLS[i].replace("*", "")) || url == URLS[i]) {
+            return URLS[i];
+        };
+    };
+};
 
 // Request a refresh when the page changes
 chrome.webRequest.onCompleted.addListener(function(details) {
+    console.log("onComplete")
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        switch (details.url) {
+        url = checkURL(details.url)
+        switch (url) {
             case FSEND:
             case MSEND:
                 chrome.tabs.sendMessage(tabs[0].id, "sent");
@@ -33,14 +55,5 @@ chrome.webRequest.onCompleted.addListener(function(details) {
                 break;
         }
     });
-}, {urls: [
-    FSEND,
-    FRECEIVE,
-    FSWITCH,
-    FSCROLL,
-    MSEND,
-    MSCROLL,
-    MSWITCH,
-    MSCROLL,
-    FTAB,
-]});
+}, {urls: URLS}
+);
