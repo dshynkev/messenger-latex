74a7f0fc9de2e27de82348d961567398e5b020ad
bretsky <bretskyselby99@gmail.com>
2017-06-25 12:46:28 -0700

    Fix URL matching

--- background.js
+++ background.js
@@ -1,16 +1,39 @@
-var FSEND = "https://www.facebook.com/messaging/send/?dpr=1"
-var FRECEIVE = "https://www.facebook.com/ajax/mercury/mark_seen.php?dpr=1"
+var FSEND = "https://www.facebook.com/messaging/send/*"
+var FRECEIVE = "https://www.facebook.com/ajax/mercury/mark_seen.php*"
 var FSWITCH = "https://www.facebook.com/ajax/bz"
 var FSCROLL = "https://www.facebook.com/api/graphqlbatch/"
-var MSEND = "https://www.messenger.com/messaging/send/?dpr=1"
-var MRECEIVE = "https://www.messenger.com/ajax/mercury/delivery_receipts.php?dpr=1"
+var MSEND = "https://www.messenger.com/messaging/send/*"
+var MRECEIVE = "https://www.messenger.com/ajax/mercury/delivery_receipts.php*"
 var MSWITCH = "https://www.messenger.com/ajax/bz"
 var MSCROLL = "https://www.messenger.com/api/graphqlbatch/"
+var FTAB = "https://www.facebook.com/ajax/mercury/tabs_presence.php*"
+
+URLS = [
+    FSEND,
+    FRECEIVE,
+    FSWITCH,
+    FSCROLL,
+    MSEND,
+    MRECEIVE,
+    MSWITCH,
+    MSCROLL,
+    FTAB,
+];
+
+function checkURL(url) {
+    for (var i = 0; i < URLS.length; i++) {
+        if (url.includes(URLS[i].replace("*", "")) || url == URLS[i]) {
+            return URLS[i];
+        };
+    };
+};
 
 // Request a refresh when the page changes
 chrome.webRequest.onCompleted.addListener(function(details) {
+    console.log("onComplete")
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        switch (details.url) {
+        url = checkURL(details.url)
+        switch (url) {
             case FSEND:
             case MSEND:
                 chrome.tabs.sendMessage(tabs[0].id, "sent");
@@ -30,13 +53,5 @@ chrome.webRequest.onCompleted.addListener(function(details) {
         }
           chrome.tabs.sendMessage(tabs[0].id, "refresh");
     });
-}, {urls: [
-    FSEND,
-    FRECEIVE,
-    FSWITCH,
-    FSCROLL,
-    MSEND,
-    MSCROLL,
-    MSWITCH,
-    MSCROLL,
-]});
+}, {urls: URLS}
+);
