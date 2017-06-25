9fce090786e822e8437d72aac92f8ef3df0e4db0
Edwin Zhang <edwinzhang64@gmail.com>
2017-06-24 22:05:45 -0600

    Cleaned up code and png.js

--- png.js
+++ png.js
@@ -40,17 +40,44 @@ window.MathJax = {
   }
 };
 
-// (function(d, script) {
-//   script = d.createElement('script');
-//   script.type = 'text/javascript';
-//   script.async = true;
-//   script.onload = function() {
-//     // remote script has loaded
-//   };
-//   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js';
-//   d.getElementsByTagName('head')[0].appendChild(script);
-// }(document));
+function render() {
+  var svg = document.getElementById('MathPreview').getElementsByClassName("MathJax_SVG")[0].childNodes[0];
+  var canvas = document.querySelector('drawCanvas');
 
-/*var canvas = document.getElementsByTagName("svg")[0];
-var img = canvas.toDataURL("image/png");
-document.write('<img src="'+img+'"/>');*/
+  function triggerDownload (imgURI) {
+    var evt = new MouseEvent('click', {
+      view: window,
+      bubbles: false,
+      cancelable: true
+    });
+
+    var a = document.createElement('a');
+    a.setAttribute('download', 'MY_COOL_IMAGE.png');
+    a.setAttribute('href', imgURI);
+    a.setAttribute('target', '_blank');
+
+    a.dispatchEvent(evt);
+  }
+
+  var canvas = document.getElementById('drawCanvas');
+    var ctx = canvas.getContext('2d');
+    var data = (new XMLSerializer()).serializeToString(svg);
+    var DOMURL = window.URL || window.webkitURL || window;
+
+    var img = new Image();
+    var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+    var url = DOMURL.createObjectURL(svgBlob);
+
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+      DOMURL.revokeObjectURL(url);
+
+      var imgURI = canvas
+          .toDataURL('image/png')
+          .replace('image/png', 'image/octet-stream');
+
+      triggerDownload(imgURI);
+    };
+
+    img.src = url;
+};
